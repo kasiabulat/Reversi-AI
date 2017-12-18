632a8cbdc11b3d51b7af75741e72342d157b3f96
Kamil Rajtar <Kamil@rajtar.pl>
2017-12-18 22:40:33 +0100

    Some check all options player enhancements

--- src/players/ai/CheckAllOptionsPlayer.kt
+++ src/players/ai/CheckAllOptionsPlayer.kt
@@ -2,20 +2,16 @@ package players.ai
 
 import board.Board
 import players.SynchronousPlayer
+import java.util.*
 import java.util.concurrent.ConcurrentHashMap
 
 /**
  * Created by Kamil Rajtar on 18.12.17.  */
-class CheckAllOptionsPlayer(override val name:String,private val depth:Int):SynchronousPlayer {
+class CheckAllOptionsPlayer(override val name:String,private val maxDepth:Int=64,private val numberOfTries:Int=1):SynchronousPlayer {
 
 	private val results=ConcurrentHashMap<Board,Int>()
-
-	private fun Board.evaluate():Int {
-		val previousResult=results[this]
-		if(previousResult!=null)
-			return previousResult
-		System.out.print(" ")
-		System.out.print(emptyFields)
+	private val random=Random()
+	private fun Board.evaluateRandom():Int {
 		if(isGameEnded()) {
 			return when(getDominatingSite()) {
 				Board.Site.PLAYER->1
@@ -23,17 +19,53 @@ class CheckAllOptionsPlayer(override val name:String,private val depth:Int):Sync
 				null->0
 			}
 		}
-
 		if(!canPlayerPutPiece()) {
-			return -passTurn().evaluate()
+			return -passTurn().evaluateRandom()
+		}
+		val moves=(0..63).filter {isCorrectMove(it)}.toList()
+		val elementNumber=random.nextInt(moves.size)
+		val playedMove=moves[elementNumber]
+		val nextBoard=makeMove(playedMove)
+		return -nextBoard.evaluateRandom()
+	}
+
+	private fun Board.evaluate(depth:Int):Int {
+		val previousResult=results[this]
+		if(previousResult!=null)
+			return previousResult
+		if(isGameEnded()) {
+			return when(getDominatingSite()) {
+				Board.Site.PLAYER->numberOfTries
+				Board.Site.OPPONENT->-numberOfTries
+				null->0
+			}
 		}
 
-		val result=-((0..63).filter {isCorrectMove(it)}.map {makeMove(it).evaluate()}.min() ?: throw RuntimeException("Cannot make move"))
+		if(!canPlayerPutPiece()) {
+			return -passTurn().evaluate(depth)
+		}
+		if(depth>maxDepth)
+		{
+			return (0..63).filter {isCorrectMove(it)}.parallelStream().map{makeMove(it) to it}.map {
+				var result=0
+				repeat(numberOfTries)
+				{_->
+					result+=it.first.evaluateRandom()
+				}
+				return@map it.second to result
+			}.min {A,B-> A.second.compareTo(B.second)}.map {it.first}.get()
+
+		}
+
+
+		val result=-((0..63).filter {isCorrectMove(it)}.map {makeMove(it).evaluate(depth+1)}.min() ?: throw RuntimeException("Cannot make move"))
 		results[this]=result
 		return result
 	}
 
 	override fun makeMove(board:Board):Int {
-		return (0..63).filter {board.isCorrectMove(it)}.parallelStream().map {board.makeMove(it).evaluate() to it}.min {a,b-> a.first.compareTo(b.first)}.get().second
+		val result=(0..63).filter {board.isCorrectMove(it)}.parallelStream().map {board.makeMove(it).evaluate(0) to it}.min {a,b-> a.first.compareTo(b.first)}.get().second
+		results.clear()
+		return result
 	}
 }
--- src/players/ai/RandomizePlayPlayer.kt
+++ src/players/ai/RandomizePlayPlayer.kt
@@ -7,34 +7,33 @@ import java.util.*
 /**
  * Created by Kamil Rajtar on 15.12.17.  */
 class RandomizePlayPlayer(override val name:String,private val random:Random,private val numberOfTries:Int):SynchronousPlayer {
-	private fun evaluateBoard(board:Board):Int {
-		if(board.isGameEnded()) {
-			return when(board.getDominatingSite()) {
+	private fun Board.evaluate():Int {
+		if(isGameEnded()) {
+			return when(getDominatingSite()) {
 				Board.Site.PLAYER->1
 				Board.Site.OPPONENT->-1
 				null->0
 			}
 		}
-		if(!board.canPlayerPutPiece()) {
-			return -evaluateBoard(board.passTurn())
+		if(!canPlayerPutPiece()) {
+			return -passTurn().evaluate()
 		}
-		val moves=(0..63).filter {board.isCorrectMove(it)}.toList()
+		val moves=(0..63).filter {isCorrectMove(it)}.toList()
 		val elementNumber=random.nextInt(moves.size)
 		val playedMove=moves[elementNumber]
-		val nextBoard=board.makeMove(playedMove)
-		return -(evaluateBoard(nextBoard))
+		val nextBoard=makeMove(playedMove)
+		return -nextBoard.evaluate()
 	}
 
 	override fun makeMove(board:Board):Int {
 
-		return (0..63).filter {board.isCorrectMove(it)}.parallelStream().map {
+		return (0..63).filter {board.isCorrectMove(it)}.parallelStream().map{board.makeMove(it) to it}.map {
 			var result=0
-			val currentBoard=board.makeMove(it)
 			repeat(numberOfTries)
-			{
-				result+=evaluateBoard(currentBoard)
+			{_->
+				result+=it.first.evaluate()
 			}
-			return@map it to result
+			return@map it.second to result
 		}.min {A,B-> A.second.compareTo(B.second)}.map {it.first}.get()
 	}
 }
