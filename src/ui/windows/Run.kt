3dfcc1c0f9f4b41dc9a2a32c97a8952686467929
Kamil Rajtar <Kamil@rajtar.pl>
2018-01-17 20:49:20 +0100

    Added jar

---
+++ src/ui/windows/Run.kt
@@ -0,0 +1,10 @@
+@file:JvmName("Launcher")
+package ui.windows
+
+import javafx.application.Application
+/**
+ * Created by Kamil Rajtar on 17.01.18.
+ */
+fun main(args:Array<String>) {
+	Application.launch(Main::class.java)
+}
